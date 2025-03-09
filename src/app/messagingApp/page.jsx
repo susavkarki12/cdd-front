27ba28e5b5c667bd7753129c8ff2f6e4d02c1593
susavkarki12 <077bei043@ioepc.edu.np>
2025-03-09 21:37:53 +0545

    sadfasdf

--- src/app/messagingApp/page.jsx
+++ src/app/messagingApp/page.jsx
@@ -32,7 +32,7 @@ function Page() {
         e.preventDefault();
 
         try {
-            const res = await fetch(`https://localhost:5000/predict`, {
+            const res = await fetch(`http://localhost:5000/predict`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ text }),
